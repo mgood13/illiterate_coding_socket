22956257d39274c88435d9b9038c3eaa18e49e25
Michael Good <michael@michaels-mbp-3.myfiosgateway.com>
2022-01-21 21:40:10 -0500

    typo user not username

--- app.py
+++ app.py
@@ -28,8 +28,8 @@ def handleMessage(data):
     print(data)
 
     if 'user' in data:
-        print('adding {} to the session'.format(data.username))
-        session['username'] = data.username
+        print('adding {} to the session'.format(data.user))
+        session['username'] = data.user
 
     emit("new_message",data,broadcast=True)
 
